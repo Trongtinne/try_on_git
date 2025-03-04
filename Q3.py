3038faa9fb18b15851ed29675717ccd6f0e2b8df
Trongtinne <149985316+Trongtinne@users.noreply.github.com>
2025-03-04 11:01:25 +0700

    Delete Q3.py

--- Q3.py
+++
@@ -1,6 +0,0 @@
-import re
-def extract_hashtag(text):
-    hashtag=re.findall(r'#\w+' , text)
-    return hashtag
-text="Please share for us and using hashtag #fptschools #SxM when you share"
-print(extract_hashtag(text))
